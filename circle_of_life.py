947b880908d533fad96354a7a673d4305c23e71c
SeokHyun Moon <seokhyunmoon@yonsei.ac.kr>
2023-05-08 17:28:12 +0900

    seokhyunmoon commit

--- circle_of_life.py
+++ circle_of_life.py
@@ -6,15 +6,15 @@ def print_TODO(todo):
 class CircleOfLife:
     def __init__(self, world_size, num_zebras, num_lions):
         self.occupancy = [[False for _ in range(world_size)]
-                          for _ in range(word_size)]
+                          for _ in range(world_size)]
         print_TODO('get random empty coordinates')
         self.zebras = [Animal(0, 0) for _ in range(num_zebras)]
         self.lions = [Animal(0, 0) for _ in range(num_lions)]
         self.timestep = 0
         print('Welcome to AIE Safari!')
         print(f'\tworld size = {world_size}')
-        print(f'\tnumber of zebras = {len{self.zebras}}')
-        print(f'\tnumber of lions = {len{self.lions}}')
+        print(f'\tnumber of zebras = {len(self.zebras)}')
+        print(f'\tnumber of lions = {len(self.lions)}')
 
     def display(self):
         print(f'Clock: {self.timestep}')
