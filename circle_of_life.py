de5357bfbbd369ae76ae52bf71cd85d2ecc0f05b
kimiyeon <miyeoni@yonsei.ac.kr>
2023-05-08 17:03:23 +0900

    dry outline

---
+++ circle_of_life.py
@@ -0,0 +1,36 @@
+from animal import Animal
+
+def print_TODO(todo):
+    print(f'<<< NOT IMPLEMENTED: {todo} >>>')
+
+class CircleOfLife:
+    def __init__(self, world_size, num_zebras, num_lions):
+        self.occupancy = [[False for _ in range(world_size)]
+                          for _ in range(word_size)]
+        print_TODO('get random empty coordinates')
+        self.zebras = [Animal(0, 0) for _ in range(num_zebras)]
+        self.lions = [Animal(0, 0) for _ in range(num_lions)]
+        print('Welcome to AIE Safari!')
+        print(f'\tworld size = {world_size}')
+        print(f'\tnumber of zebras = {len{self.zebras}}')
+        print(f'\tnumber of lions = {len{self.lions}}')
+
+    def display(self):
+        print_TODO('display()')
+        key = input('enter [q] to quit:')
+        if key == 'q':
+            exit()
+
+    def step_move(self):
+        print_TODO('step_move()')
+        for zebra in self.zebras:
+            print_TODO('get empty neighbor')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    safari = CirleOfLife(5, 5, 2)
