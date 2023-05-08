8ba0083d84f94ae2e0b70724193771fc71342eed
kimiyeon <miyeoni@yonsei.ac.kr>
2023-05-08 17:12:14 +0900

    dry outline

--- circle_of_life.py
+++ circle_of_life.py
@@ -10,12 +10,14 @@ class CircleOfLife:
         print_TODO('get random empty coordinates')
         self.zebras = [Animal(0, 0) for _ in range(num_zebras)]
         self.lions = [Animal(0, 0) for _ in range(num_lions)]
+        self.timestep = 0
         print('Welcome to AIE Safari!')
         print(f'\tworld size = {world_size}')
         print(f'\tnumber of zebras = {len{self.zebras}}')
         print(f'\tnumber of lions = {len{self.lions}}')
 
     def display(self):
+        print(f'Clock: {self.timestep}')
         print_TODO('display()')
         key = input('enter [q] to quit:')
         if key == 'q':
@@ -25,8 +27,30 @@ class CircleOfLife:
         print_TODO('step_move()')
         for zebra in self.zebras:
             print_TODO('get empty neighbor')
-
-
+            direction = 'left'
+            zebra.move(direction)
+        for lion in self.lions:
+            print_TODO('get neighboring zebra')
+            print_TODO('move to zebra if found, else move to empty')
+            print_TODO('get empty neighbor')
+            direction = 'left'
+            lion.move(direction)
+    
+    def step_breed(self):
+        print_TODO('step_breed()')
+        for animal in self.zebras + self.lions:
+            print_TODO('get empty neighbor')
+            x, y = 0, 0
+            animal.breed(x, y)
+    
+    def run(self, num_timesteps=100):
+        self.display()
+        for _ in range(num_timesteps):
+            self.timestep +=1
+            self.step_move()
+            self.display()
+            self.step_breed()
+            self.display()
 
 
 
@@ -34,3 +58,4 @@ class CircleOfLife:
 
 if __name__ == '__main__':
     safari = CirleOfLife(5, 5, 2)
+    safari.run(2)
